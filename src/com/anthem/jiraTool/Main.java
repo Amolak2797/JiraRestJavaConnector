79a64bda4921f7838a3d5a55b9309612a5be5cb4
Amolak2797 <amolak.hunjan.as37@gmail.com>
2018-10-29 13:16:12 +0530

    code changed to read the config file

--- src/com/anthem/jiraTool/Main.java
+++ src/com/anthem/jiraTool/Main.java
@@ -1,17 +1,31 @@
 package com.anthem.jiraTool;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception{
 		
-		String hostName=args[0];
-		String username=args[1];
-		String password=args[2];
-		String handOffFileName=args[3];
+		String propertiesFileName=System.getProperty("user.home")+"\\config.properties";
 		
+		System.out.println(propertiesFileName);
+		Properties prop=new Properties();
+		InputStream input=null;
+		input=new FileInputStream(propertiesFileName);
+		prop.load(input);
+		
+		String hostName=prop.getProperty("HOST_NAME");
+		String username=prop.getProperty("USERNAME");
+		String password=prop.getProperty("PASSWORD");
+		String handOffFileName=prop.getProperty("HANDOFF_FILE");
+		
+		System.out.println(hostName+"\t"+username+"\t"+password+"\t"+handOffFileName);
+				
 		HandOffExcelReader excelReader=new HandOffExcelReader(handOffFileName);
 		System.out.println(excelReader.getJiraIssues());
 		
